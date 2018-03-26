70bcc60c58438722e5ef5f4dd2c370910c574e95
K <bistok@ya.ru>
2018-03-27 00:36:43 +0300

    Deploy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-/*INSERT INTO ROLE (ID, NAME , AUTHORITY) VALUES
+INSERT INTO ROLE (ID, NAME , AUTHORITY) VALUES
   (1, 'Users', 'ROLE_USER'),
   (2, 'Moderators', 'ROLE_MODERATOR'),
-  (3, 'Administrators', 'ROLE_ADMIN');*/
+  (3, 'Administrators', 'ROLE_ADMIN');
