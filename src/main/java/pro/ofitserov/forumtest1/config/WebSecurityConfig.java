1ac2eecbf8ddff9da11166b21ce9d06a9f4452a9
K <bistok@ya.ru>
2018-04-02 01:16:03 +0300

    Deploy

--- src/main/java/pro/ofitserov/forumtest1/config/WebSecurityConfig.java
+++ src/main/java/pro/ofitserov/forumtest1/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package pro.ofitserov.forumtest1.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
